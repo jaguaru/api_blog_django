e8983ac43f582ab6347516051adf8a400bb01acf
jaguaru <jaguaru@outlook.com>
2024-09-11 13:38:40 -0600

    Fixing models Post and Comment in settings.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,14 +2,14 @@ from rest_framework import serializers
 from .models import Post, Comment
 
 
-# Serializer for the User model
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
-
-# Serializer for the Post model
+# Serializador para el modelo Post
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'created_at', 'updated_at']
+
+# Serializador para el modelo Comment
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'post', 'content', 'created_at', 'updated_at']
