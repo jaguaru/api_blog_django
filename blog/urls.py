1262c91e91efc3cfbf6c4594700e564dc3cab492
jaguaru <jaguaru@outlook.com>
2024-09-11 14:49:10 -0600

    Fixing Post and Comment api routes in urls.py and Adding to path urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PostViewSet, CommentViewSet
+from .api import PostViewSet, CommentViewSet
 
 # Create a router and register our viewsets with it
 router = DefaultRouter()
 
-router.register(r'posts', PostViewSet)  # Register PostViewSet
-router.register(r'comments', CommentViewSet)  # Register CommentViewSet
-#router.register('api/posts', PostViewSet, 'posts')  # Register PostViewSet
-#router.register('api/comments', CommentViewSet, 'comments')  # Register CommentViewSet
+router.register('api/blog/posts', PostViewSet, 'posts')  # Register PostViewSet
+router.register('api/blog/comments', CommentViewSet, 'comments')  # Register CommentViewSet
 
 # The API URLs are now determined automatically by the router
-urlpatterns = [
-    path('api/blog', include(router.urls)),  # Include the generated URLs from the router
-]
+urlpatterns = router.urls
 
