0abba86752b48bc12164893d817a01f69e79231d
jaguaru <jaguaru@outlook.com>
2024-09-11 13:25:52 -0600

    Adding models Post and Comment in settings.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+# Model to create Post table
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+# Model to create Comment table
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment on {self.post.title}"
