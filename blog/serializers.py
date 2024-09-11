0522c7b8dc385d43381d5e6a5be55d6723524cef
jaguaru <jaguaru@outlook.com>
2024-09-11 13:30:07 -0600

    Adding serializers Post and Comment in serializers.py

---
+++ blog/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Post, Comment
+
+
+# Serializer for the User model
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+# Serializer for the Post model
+class PostSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'created_at', 'updated_at']
