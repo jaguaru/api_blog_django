b503900fd96292dd8b2bdf9022eaa768a0d4ec55
jaguaru <jaguaru@outlook.com>
2024-09-11 19:58:11 -0600

    Adding UserRegisterView in api.py

--- blog/api.py
+++ blog/api.py
@@ -1,11 +1,22 @@
 
+from rest_framework import status
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
 from rest_framework.response import Response
 from rest_framework.decorators import action
+from rest_framework.views import APIView
 from .models import Post, Comment
-from .serializers import PostSerializer, CommentSerializer
+from .serializers import PostSerializer, CommentSerializer, UserRegisterSerializer
 
+
+# RegisterView for UserRegister model
+class UserRegisterView(APIView):
+    def post(self, request, *args, **kwargs):
+        serializer = UserRegisterSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()  # Guarda el nuevo usuario
+            return Response({"message": "Usuario creado correctamente"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # ViewSet for Post model
 class PostViewSet(viewsets.ModelViewSet):
