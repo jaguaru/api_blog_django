77c7608b60bbd419f87b7453d444d7ad2a57fdb0
jaguaru <jaguaru@outlook.com>
2024-09-11 19:53:30 -0600

    Adding api/token routes in urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .api import PostViewSet, CommentViewSet
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
 
 # Create a router and register our viewsets with it
 router = DefaultRouter()
@@ -8,6 +10,8 @@ router = DefaultRouter()
 router.register('api/blog/posts', PostViewSet, 'posts')  # Register PostViewSet
 router.register('api/blog/comments', CommentViewSet, 'comments')  # Register CommentViewSet
 
-# The API URLs are now determined automatically by the router
-urlpatterns = router.urls
-
+urlpatterns = [
+    path('api/', include(router.urls)),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
