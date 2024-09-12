93833763cba017b1003e101048e1ee65b2365188
jaguaru <jaguaru@outlook.com>
2024-09-11 19:24:43 -0600

    Adding author field in Comment model in models.py

--- blog/models.py
+++ blog/models.py
@@ -17,6 +17,7 @@ class Post(models.Model):
 # Model to create Comment table
 class Comment(models.Model):
     post = models.ForeignKey(Post, related_name='comments', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
