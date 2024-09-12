291151d8182e6b37732e010b6710234c72ddc137
jaguaru <jaguaru@outlook.com>
2024-09-11 19:43:11 -0600

    Adding perform_create and comments attributes in PostViewSet in api.py

--- blog/api.py
+++ blog/api.py
@@ -1,5 +1,8 @@
+
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, AllowAny
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
 
@@ -11,6 +14,16 @@ class PostViewSet(viewsets.ModelViewSet):
     # Only authenticated users can create, update, and delete. Read-only access for others.
     #permission_classes = [IsAuthenticatedOrReadOnly]
     permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+    @action(detail=True, methods=['get'])
+    def comments(self, request, pk=None):
+        post = self.get_object()
+        comments = post.comments.all()
+        serializer = CommentSerializer(comments, many=True)
+        return Response(serializer.data)
 
 # ViewSet for Comment model
 class CommentViewSet(viewsets.ModelViewSet):
