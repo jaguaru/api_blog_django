5381519d42e61993f8e0ebf4278480488446c9a5
jaguaru <jaguaru@outlook.com>
2024-09-11 20:33:24 -0600

    Creating class IsAuthorOrReadOnly and changing permission_classes in api.py

--- blog/api.py
+++ blog/api.py
@@ -9,6 +9,14 @@ from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer, UserRegisterSerializer
 
 
+# Permission to modify only if you are the author
+class IsAuthorOrReadOnly(permissions.BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Only allow update or deletion if the user is the author
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return obj.author == request.user
+
 # RegisterView for UserRegister model
 class UserRegisterView(APIView):
     def post(self, request, *args, **kwargs):
@@ -23,8 +31,8 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
     # Only authenticated users can create, update, and delete. Read-only access for others.
-    #permission_classes = [IsAuthenticatedOrReadOnly]
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    #permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -41,7 +49,8 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     # Only authenticated users can create, update, and delete. Read-only access for others.
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    #permission_classes = [AllowAny]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
