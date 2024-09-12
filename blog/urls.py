c777bde8146cde8a56399e1ad3465e19097bc179
jaguaru <jaguaru@outlook.com>
2024-09-11 20:02:11 -0600

    Adding UserRegisterView import in api.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .api import PostViewSet, CommentViewSet
+from .api import PostViewSet, CommentViewSet, UserRegisterView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 
