7ee4c00818ba1c8eb88305421c4013485ae25314
jaguaru <jaguaru@outlook.com>
2024-09-11 19:31:27 -0600

    Changing order serializers in serializers.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,15 +2,6 @@ from rest_framework import serializers
 from .models import Post, Comment
 
 
-# Serializer for Post model
-class PostSerializer(serializers.ModelSerializer):
-    author = serializers.StringRelatedField()
-    comments = CommentSerializer(many=True, read_only=True)
-
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'comments']
-        read_only_field = ['created_at', 'updated_at']
 
 # Serializer for Comment model
 class CommentSerializer(serializers.ModelSerializer):
@@ -19,4 +10,14 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = ['id', 'author', 'post', 'content', 'created_at', 'updated_at']
+        read_only_field = ['created_at', 'updated_at']
+
+# Serializer for Post model
+class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    comments = CommentSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Post
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'comments']
         read_only_field = ['created_at', 'updated_at']
