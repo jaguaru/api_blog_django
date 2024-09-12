207bd90079ae8785a1b286003cbb98b406924a6e
jaguaru <jaguaru@outlook.com>
2024-09-11 19:54:20 -0600

    Adding UserRegisterSerializer model in serializers.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -2,6 +2,25 @@ from rest_framework import serializers
 from .models import Post, Comment
 
 
+# Serializer for UserRegister model
+class UserRegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+    def create(self, validated_data):
+        # Create a new user with validated data
+        user = User(
+            username=validated_data['username'],
+            email=validated_data.get('email', '')
+        )
+        # Set the password correctly using set_password
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
 # Serializer for Comment model
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.StringRelatedField()
