bb739920d1a076dcebcb2952127d375ec1fa0547
jaguaru <jaguaru@outlook.com>
2024-09-11 19:29:03 -0600

    Adding author and comments field in PostSerializer in serializers.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -4,9 +4,12 @@ from .models import Post, Comment
 
 # Serializer for Post model
 class PostSerializer(serializers.ModelSerializer):
+    author = serializers.StringRelatedField()
+    comments = CommentSerializer(many=True, read_only=True)
+
     class Meta:
         model = Post
-        fields = ['id', 'title', 'content', 'created_at', 'updated_at']
+        fields = ['id', 'title', 'content', 'author', 'created_at', 'updated_at', 'comments']
         read_only_field = ['created_at', 'updated_at']
 
 # Serializer for Comment model
