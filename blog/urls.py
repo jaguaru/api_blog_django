590b64b077713cb7158e1933730154690aa245b9
jaguaru <jaguaru@outlook.com>
2024-09-11 20:00:22 -0600

    Changing route name in api.py

--- blog/urls.py
+++ blog/urls.py
@@ -7,11 +7,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 # Create a router and register our viewsets with it
 router = DefaultRouter()
 
-router.register('api/blog/posts', PostViewSet, 'posts')  # Register PostViewSet
-router.register('api/blog/comments', CommentViewSet, 'comments')  # Register CommentViewSet
+router.register('blog/posts', PostViewSet, 'posts')  # Register PostViewSet
+router.register('blog/comments', CommentViewSet, 'comments')  # Register CommentViewSet
 
 urlpatterns = [
     path('api/', include(router.urls)),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
+
