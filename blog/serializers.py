0ef895ea68e04669975a170f9f59446398c96673
jaguaru <jaguaru@outlook.com>
2024-09-11 18:44:24 -0600

    Changing the comments of the Post and Comment models in serializers.py

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,15 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Post, Comment
 
 
-# Serializador para el modelo Post
+# Serializer for User model
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+# Serializer for Post model
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'created_at', 'updated_at']
         read_only_field = ['created_at', 'updated_at']
 
-# Serializador para el modelo Comment
+# Serializer for Comment model
 class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
