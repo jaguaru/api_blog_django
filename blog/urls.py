3c3d551cce6bf00b97fb5d4fc5996fbf49106d01
jaguaru <jaguaru@outlook.com>
2024-09-11 20:01:26 -0600

    Changing register route in api.py

--- blog/urls.py
+++ blog/urls.py
@@ -12,6 +12,9 @@ router.register('blog/comments', CommentViewSet, 'comments')  # Register Comment
 
 urlpatterns = [
     path('api/', include(router.urls)),
+
+    path('api/register/', UserRegisterView.as_view(), name='register'),
+
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
