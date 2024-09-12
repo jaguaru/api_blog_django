25bc76971f3e24790182ed3b9796caeb2cee3077
jaguaru <jaguaru@outlook.com>
2024-09-11 20:15:35 -0600

    Changing message response in UserRegisterView in api.py

--- blog/api.py
+++ blog/api.py
@@ -14,8 +14,8 @@ class UserRegisterView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = UserRegisterSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()  # Guarda el nuevo usuario
-            return Response({"message": "Usuario creado correctamente"}, status=status.HTTP_201_CREATED)
+            serializer.save()
+            return Response({"message": "Successfully created user!"}, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 # ViewSet for Post model
